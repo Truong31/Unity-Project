413c7829b0b7ce119f1a929fc790c94e0672844c
Truong31 <lxtruong3107@gmail.com>
2024-08-25 00:11:05 +0700

    Unity Project

---
+++ Bomberman/Assets/Scripts/AnimatedSpriteRenderer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedSpriteRenderer : MonoBehaviour
+{
+    public Sprite idleSprite;       //Sprite khi nhan vat dung yen
+    public Sprite[] animationSprites;
+
+    private SpriteRenderer spriteRenderer;
+    private int currentSprite = 0;
+    private float animationTime = 0.25f;
+
+    public bool loop = true;
+    public bool idle = true;        //Check xem nhan vat co dang dung yen
+
+    private void Awake()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Start()
+    {
+        InvokeRepeating(nameof(NextFrame), this.animationTime, this.animationTime);
+    }
+
+    private void OnEnable()
+    {
+        this.spriteRenderer.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        this.spriteRenderer.enabled = false;
+    }
+    private void NextFrame()
+    {
+        this.currentSprite++;
+        if(loop && this.currentSprite >= this.animationSprites.Length)
+        {
+            this.currentSprite = 0;
+        }
+        if (idle)
+        {
+            this.spriteRenderer.sprite = this.idleSprite;
+        }
+        else if(this.currentSprite < this.animationSprites.Length && this.currentSprite >= 0)
+        {
+            this.spriteRenderer.sprite = this.animationSprites[currentSprite];
+        }
+    }
+}
---
+++ Bomberman/Assets/Scripts/MovementController.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    private Vector2 direction = Vector2.down;
+    public float speed = 5.0f;
+
+    public KeyCode inputDown = KeyCode.S;
+    public KeyCode inputUp = KeyCode.W;
+    public KeyCode inputLeft = KeyCode.A;
+    public KeyCode inputRight = KeyCode.D;
+
+    public AnimatedSpriteRenderer spriteRendererUp;
+    public AnimatedSpriteRenderer spriteRendererDown;
+    public AnimatedSpriteRenderer spriteRendererLeft;
+    public AnimatedSpriteRenderer spriteRendererRight;
+    private AnimatedSpriteRenderer activeSpriteRenderer;
+
+
+    private void Awake()
+    {
+        this.rigidbody = GetComponent<Rigidbody2D>();
+        this.activeSpriteRenderer = this.spriteRendererDown;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(inputDown))
+        {
+            SetDirection(Vector2.down, this.spriteRendererDown);
+        }
+        else if (Input.GetKey(inputUp))
+        {
+            SetDirection(Vector2.up, this.spriteRendererUp);
+        }
+        else if (Input.GetKey(inputLeft))
+        {
+            SetDirection(Vector2.left, this.spriteRendererLeft);
+        }
+        else if (Input.GetKey(inputRight))
+        {
+            SetDirection(Vector2.right, this.spriteRendererRight);
+        }
+        else
+        {
+            SetDirection(Vector2.zero, this.activeSpriteRenderer);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = this.rigidbody.position;
+        Vector2 translation = this.direction * speed * Time.fixedDeltaTime;
+
+        this.rigidbody.MovePosition(position + translation);
+    }
+
+    private void SetDirection(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
+    {
+        this.direction = newDirection;
+
+        this.spriteRendererDown.enabled = spriteRenderer == this.spriteRendererDown;
+        this.spriteRendererLeft.enabled = spriteRenderer == this.spriteRendererLeft;
+        this.spriteRendererRight.enabled = spriteRenderer == this.spriteRendererRight;
+        this.spriteRendererUp.enabled = spriteRenderer == this.spriteRendererUp;
+        this.activeSpriteRenderer = spriteRenderer;
+
+        this.activeSpriteRenderer = spriteRenderer;
+        this.activeSpriteRenderer.idle = direction == Vector2.zero;
+
+        //Cach hoat dong:
+
+        //if (spriteRenderer == this.spriteRendererDown)
+        //{
+        //    this.spriteRendererDown.enabled = true;
+        //    this.spriteRendererLeft.enabled = false;
+        //    this.spriteRendererRight.enabled = false;
+        //    this.spriteRendererUp.enabled = false;
+        //}
+        //else if (spriteRenderer == this.spriteRendererUp)
+        //{
+        //    this.spriteRendererDown.enabled = false;
+        //    this.spriteRendererLeft.enabled = false;
+        //    this.spriteRendererRight.enabled = false;
+        //    this.spriteRendererUp.enabled = true;
+        //}
+        //else if (spriteRenderer == this.spriteRendererLeft)
+        //{
+        //    this.spriteRendererDown.enabled = false;
+        //    this.spriteRendererLeft.enabled = true;
+        //    this.spriteRendererRight.enabled = false;
+        //    this.spriteRendererUp.enabled = false;
+        //}
+        //else if (spriteRenderer == this.spriteRendererRight)
+        //{
+        //    this.spriteRendererDown.enabled = false;
+        //    this.spriteRendererLeft.enabled = false;
+        //    this.spriteRendererRight.enabled = true;
+        //    this.spriteRendererUp.enabled = false;
+        //}
+        //this.activeSpriteRenderer = spriteRenderer;
+        //if (this.direction == Vector2.zero)
+        //{
+        //    this.activeSpriteRenderer.idle = true;
+        //}
+        //else
+        //{
+        //    this.activeSpriteRenderer.idle = false;
+        //}
+    }
+}
